15d4d2c12f923c1205335e278c61a3c89f9beb6b
Estherpyr <60486583+Estherpyr@users.noreply.github.com>
2020-04-05 16:55:34 -0400

    upload

---
+++ chrome-extention-mashup/README.md
@@ -0,0 +1,6 @@
+HEYNYU
+==========
+
+This is the chrome extention that I created to show my love to NYU.
+ All the images on the webpage will be changed into the images of nyu
+ and the background color of the text will be changed into violet. 
---
+++ chrome-extention-mashup/swap.js
@@ -0,0 +1,32 @@
+﻿var images = [
+  "https://www.google.com/search?q=nyu&sxsrf=ALeKk03HjGWscGFvURbC0eviLZAcoLF-WA:1586118505978&source=lnms&tbm=isch&sa=X&ved=2ahUKEwjh-_rwj9LoAhXAAp0JHccGD7MQ_AUoA3oECAsQBQ&biw=1440&bih=732#imgrc=AZYRYAKMcVpiIM",
+  "https://www.google.com/search?q=nyu&sxsrf=ALeKk03HjGWscGFvURbC0eviLZAcoLF-WA:1586118505978&source=lnms&tbm=isch&sa=X&ved=2ahUKEwjh-_rwj9LoAhXAAp0JHccGD7MQ_AUoA3oECAsQBQ&biw=1440&bih=732#imgrc=f2c-2w79IBBuUM",
+  "https://www.google.com/search?q=nyu&sxsrf=ALeKk03HjGWscGFvURbC0eviLZAcoLF-WA:1586118505978&source=lnms&tbm=isch&sa=X&ved=2ahUKEwjh-_rwj9LoAhXAAp0JHccGD7MQ_AUoA3oECAsQBQ&biw=1440&bih=732#imgrc=WFXWlaPfNUgZVM",
+  "https://www.google.com/search?q=nyu&sxsrf=ALeKk03HjGWscGFvURbC0eviLZAcoLF-WA:1586118505978&source=lnms&tbm=isch&sa=X&ved=2ahUKEwjh-_rwj9LoAhXAAp0JHccGD7MQ_AUoA3oECAsQBQ&biw=1440&bih=732#imgrc=SUm11leC_hNUDM",
+  "https://www.google.com/search?q=nyu&sxsrf=ALeKk03HjGWscGFvURbC0eviLZAcoLF-WA:1586118505978&source=lnms&tbm=isch&sa=X&ved=2ahUKEwjh-_rwj9LoAhXAAp0JHccGD7MQ_AUoA3oECAsQBQ&biw=1440&bih=732#imgrc=f2c-2w79IBBuUM"
+];
+
+console.log("extention's right there man");
+
+window.onload = replaceImages;
+
+function replaceImages() {
+    for (var i in document.images) {
+        var targetImage = document.images[i];
+        var randomImage = images[Math.floor(Math.random() * images.length)];
+        var height = targetImage.height;
+        var width = targetImage.width;
+
+        targetImage.src = randomImage;
+        targetImage.height = height;
+        targetImage.width = width;
+    }
+
+let words = document.getElementsByTagName('txt');
+
+for(let i = 0; i< words.length; i++){
+  words.style['background-color'] = 'violet';
+}
+
+
+}
